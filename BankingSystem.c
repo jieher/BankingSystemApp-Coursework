7837f325cdeb2c934ea40cae9e7a2a38c9c74353
Jieh Er <jieherkow@gmail.com>
2025-11-08 18:32:32 +0800

    8/11: - Leads to functions by comparing first 5 characters of input WIP: - invalid input without numbers (eg: sihufbn)

--- BankingSystem.c
+++ BankingSystem.c
@@ -2,49 +2,71 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-int checkAction(char *userInput){
+void create(){
+    printf("This is create");
+}
+
+void delete(){
+    printf("This is delete");
+}
+
+void deposit(){
+    printf("This is deposit");
+}
+
+void withdrawal(){
+    printf("This is withdrawal");
+}
+
+void remittance(){
+    printf("This is remmittance");
+}
+
+void checkAction(char *userInput, int *valid, int *action){
+    int *validPtr=valid;
+    *validPtr=1;
+    int *actionPtr=action; 
     char *inputNoSp=userInput;
     //Input with no spaces
     char *ogInput=userInput;
     //Means Original Input
     //Check for spaces and symbols
-    int i=0;
-    int a=0;
+    int index_of_ogInput=0;
     int inputLength=strlen(userInput);
-    do {
-        // while (*ogInput == ' ' || *ogInput=='/' || *ogInput==')') {
-        //     ++ogInput;
-        // }
-        if (inputLength==i){
-            while (!isalnum(*ogInput)){
-                ++ogInput;
-                a++;
-            }
-            if (a>0){
-                i+=a;
-                a=0;
-            }
-            //Yes, this complicated thing, I CREATED MYSELF WITHOUT USING AI!!!!
+    while (*ogInput != '\0') {
+        if (!isalnum(*ogInput)){
+            ogInput++;
+            continue;
         }
-    } while (*inputNoSp++ = *ogInput++);
-    printf("%s\n",userInput);
-    char actionno[100];
-    a=0;
-    for(int i=0; userInput[i]; i++){
-        if (a<=1){
-            if (isdigit(userInput[i])){
-                actionno[a]=userInput[i];
-                a++;
-            }
-            userInput[i] = tolower(userInput[i]);
-        } else {
-            printf("Error: Please enter number 1~6 only.\n");
-            return 0;
-        }
+        *inputNoSp++ = *ogInput++;
+        // Copy the character one by one
     }
-    printf("%s %s",userInput,actionno);
+    *inputNoSp='\0';
+    printf("%s\n",userInput);
+
+    int a=0;
+    int i=0;
+    for(i=0; userInput[i]!='\0'; i++){
+        if (isdigit(userInput[i])){
+            if (a==0){
+                *actionPtr=userInput[i]- '0';
+                a++;
+            } else {
+                printf("Error: Please enter number 1~6 only.\n");
+                *validPtr=0;
+            } 
+        }
+        else {
+            userInput[i-a] = tolower(userInput[i]);
+            continue;
+        } 
+    }
+    userInput[i-1]='\0';
+    printf("%s %d\n\n",userInput,*actionPtr);
     //add check for user input validity and action here
+    // *validPtr=1;
 }
+
 void menu(){
     char userInput[100];
     char action1[]="1. Create New Bank Account";
@@ -52,8 +74,10 @@ void menu(){
     char action3[]="3. Deposit";
     char action4[]="4. Withdrawal";
     char action5[]="5. Remittance";
+    char *actionlist[]={action1,action2,action3,action4,action5};
     int valid=0;
-    while (!valid){
+    int action=0;
+    while (valid==0){
         printf("====================================\n");
         printf("Choose you action:\n");
         printf("%s\n%s\n%s\n%s\n%s\n",action1,action2,action3,action4,action5);
@@ -62,8 +86,46 @@ void menu(){
         printf("Your action:");
         scanf("%[^\n]",userInput);
         while((getchar()) != '\n');
-        //eg: input=1) Check New
-        int valid=checkAction(userInput);
+        //eg: input=1) Check Ne"
+        checkAction(userInput,&valid,&action);
+    }
+    printf("%s %d %d",userInput,valid,action);
+    int tempAction=0;
+    for (int i=0;i<sizeof(actionlist)/sizeof(actionlist[0]);i++){
+        checkAction(actionlist[i],&valid,&tempAction);
+    }
+    // for (int i=0;i<sizeof(actionlist)/sizeof(actionlist[0]);i++){
+    //     printf("%s",actionlist[i]);
+    // }
+
+    if (action==0){
+        int validity=0;
+        action=1;
+        while (validity<5){
+            if (action<=5){
+                for (int i=0; i<5; i++){
+                    if (userInput[i]==actionlist[i][i]){
+                        validity++;
+                    }
+                }
+                if (validity<5){
+                    action++;
+                }
+            } 
+        }
+    }
+    if (action!=0){
+        if (action==1){
+            create();
+        } else if (action==2){
+            delete();
+        } else if (action==3){
+            deposit();
+        } else if (action==4){
+            withdrawal();
+        } else if (action==5){
+            remittance();
+        }
     }
 }
 int main(){
